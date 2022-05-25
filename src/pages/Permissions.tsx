b2d97c17118691411808a630298fea589afb4445
Bilal Rana <bilalmanj9@gmail.com>
2022-05-26 04:17:46 +0500

    Modal Added

--- src/pages/Permissions.tsx
+++ src/pages/Permissions.tsx
@@ -1,19 +1,27 @@
 import React, { ReactElement, FC } from "react";
 import { Box, Typography } from "@mui/material";
+import App from "../components/modal/permissions/Modal"
+import Drawer from "../components/Drawer"
+
 
 const Permissions: FC<any> = (): ReactElement => {
   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        backgroundColor: "whitesmoke",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Typography variant="h3">Permission Page</Typography>
-    </Box>
+    <>
+    <div className="drawer-index">
+            <Drawer />
+            </div>
+
+   <div className="container">
+     <div className="row">
+       <div className="col-6 mx-auto box-index mt-5">
+         <App />
+
+       </div>
+
+     </div>
+
+   </div>
+   </>
   );
 };
 
