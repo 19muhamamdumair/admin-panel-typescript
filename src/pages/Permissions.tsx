37729fb1d8ea98e60cf774a7b6ee8d6d05779e8b
Bilal Rana <bilalmanj9@gmail.com>
2022-05-27 03:19:50 +0500

    modal popup work 50% done

--- src/pages/Permissions.tsx
+++ src/pages/Permissions.tsx
@@ -1,27 +1,41 @@
 import React, { ReactElement, FC } from "react";
 import { Box, Typography } from "@mui/material";
-import App from "../components/modal/permissions/Modal"
 import Drawer from "../components/Drawer"
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import BasicModal from "../components/modal/permissions/PermissionModal"
+
 
 
 const Permissions: FC<any> = (): ReactElement => {
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+ 
+
   return (
     <>
-    <div className="drawer-index">
-            <Drawer />
-            </div>
+      <div className="drawer-index">
+        <Drawer />
+      </div>
+      <Grid container spacing={2}>
+        <Grid xs={4}>
+          <Item>xs=8</Item>
+        </Grid>
+        <Grid item xs={8} mt={10}>
+          <BasicModal  />
+        </Grid>
 
-   <div className="container">
-     <div className="row">
-       <div className="col-6 mx-auto box-index mt-5">
-         <App />
+      </Grid>
 
-       </div>
 
-     </div>
-
-   </div>
-   </>
+    </>
   );
 };
 
